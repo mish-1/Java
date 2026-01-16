5c6dc0bfb201251475ef82e5fa562d41186d58ca
MISHTI KINKER <mishtikinker@gmail.com>
2026-01-16 15:59:45 +0530

    Implement reverse array printing in Main class

---
+++ revarray.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int k = 0;
+        char[] arr = new char[6];
+        char name[] = {'m','i','s','h','t','i'};
+
+        for (int i = 5; i >= 0; i--) {
+            arr[k++] = name[i];
+        }
+
+        System.out.println(arr);
+    }
+}
