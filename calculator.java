52ac9faffabcd2e927d926efb61de59deb72357d
MISHTI KINKER <mishtikinker@gmail.com>
2026-01-16 14:30:40 +0530

    Add basic calculator functionality
    Implement basic calculator functions for addition, subtraction, multiplication, and division.

---
+++ calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
+    }
+    public static int mul(int a, int b){
+        return a+b;
+    }
+    public static int div(int a, int b){
+        return a/b;
+    }
+
+    public static void main(){
+        System.out.println("Calculator:\n");
+        int a =10;
+        int b=5;
+        int sum = a+b;
+        System.out.println(sum);
+    }    
+}
