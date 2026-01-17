7a086b105919a3b8e26803d38d2f17c4fef4d716
MISHTI KINKER <mishtikinker@gmail.com>
2026-01-17 11:49:51 +0530

    Add infoprint class to display user information

---
+++ infoprint.java
@@ -0,0 +1,10 @@
+class infoprint {
+   infoprint() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Name: Mishti Kinker");
+      System.out.println("Address: Pune, Maharashtra");
+      System.out.println("Roll No: 23");
+   }
+}
