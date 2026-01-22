c80b5d8d2a36e9a4b23d019ff1a578d09d19fcb6
MISHTI KINKER <mishtikinker@gmail.com>
2026-01-23 03:17:28 +0530

    Add StaticDemo class with college display method

---
+++ saticwala.java
@@ -0,0 +1,15 @@
+class StaticDemo {
+
+    
+    static String collegeName = "CMR College of Engineering";
+
+    
+    static void displayCollege() {
+        System.out.println("College Name: " + collegeName);
+    }
+
+    public static void main(String[] args) {
+        
+        StaticDemo.displayCollege();
+    }
+}
