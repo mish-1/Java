c3df9cfb4630f6e7c87081f1d4d8a833823d26e8
Mishti Kinker <mishtikinker@gmail.com>
2026-02-02 09:30:27 +0530

    Create finalvariableexample.java

---
+++ finalvariableexample.java
@@ -0,0 +1,10 @@
+public class finalvariableexample {
+    final int MAX_VALUE = 100;
+    void display() {
+        System.out.println("Max Value: " + MAX_VALUE);
+    }
+    public static void main(String[] args) {
+        finalvariableexample obj = new finalvariableexample();
+        obj.display();
+    }
+}
